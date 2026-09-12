f8df3cfab6caab4f3ad1a35d7695beb239595885
greg <greg.micah.jensen@gmail.com>
2026-09-12 18:20:02 -0400

    quote_extraction microservice rewrite init

---
+++ quote_extraction/main_test.py
@@ -0,0 +1,13 @@
+import main as sut
+import pytest
+import pytest_mock
+from typer.testing import CliRunner
+
+
+runner = CliRunner()
+
+
+def test_app():
+    result = runner.invoke(sut.app, ["--port=123"])
+    assert result.exit_code == 0
+    assert "Serving on port 123\n" == result.output
