d8ccc11b2be9862c836ee24f6ebcabcf40a48085
greg <greg.micah.jensen@gmail.com>
2026-09-12 20:54:49 -0400

    cli runs api with reloading if RUN_MODE != production

--- quote_extraction/cli.py
+++ quote_extraction/cli.py
@@ -1,11 +1,15 @@
 import typer
 import uvicorn
 import api # Technically not necessary, but I like it. See "api:app" in this file
-
+import os
 
 app = typer.Typer()
 
 
+def should_reload():
+    return (os.environ.get("RUN_MODE") != "production")
+
+
 @app.command()
 def serve(port: int = 8080):
-    uvicorn.run("api:app", host="127.0.0.1", port=port)
+    uvicorn.run("api:app", host="127.0.0.1", port=port, reload=should_reload())
--- quote_extraction/cli_test.py
+++ quote_extraction/cli_test.py
@@ -1,6 +1,7 @@
 import cli as sut
 import pytest
 from pytest_mock import MockerFixture
+from unittest.mock import MagicMock
 from typer.testing import CliRunner
 from typer.testing import Result as TyperResult
 from typing import Sequence
@@ -17,7 +18,7 @@ def mocker(pytestconfig: pytest.Config):
     mocker.stopall()
 
 
-def patch_unvicorn(mocker: MockerFixture, stub=None):
+def patch_unvicorn(mocker: MockerFixture, stub: MagicMock|None=None):
     mocker.patch('uvicorn.run', new=stub)
 
 
@@ -32,12 +33,17 @@ def assert_result_sucess(result: TyperResult):
     assert result.exit_code == 0
 
 
-def assert_runs_with_port(runner: CliRunner, mocker: MockerFixture, port: int | None, args: Sequence[str]):
+def assert_runs_with_port(runner: CliRunner, mocker: MockerFixture, port: int|None, args: Sequence[str]):
     (stub, result) = run_patched_app(runner, mocker, args)
 
     assert_result_sucess(result)
     assert len(stub.call_args_list) == 1
     assert stub.call_args_list[0].kwargs["port"] == port
+
+
+def assert_runs_with_reload(stub: MagicMock, _reload: bool):
+    assert len(stub.call_args_list) == 1
+    assert stub.call_args_list[0].kwargs.get("reload") == _reload
 
 
 def test__cli__runs_app_on_localhost(runner, mocker):
@@ -55,3 +61,19 @@ def test__cli__default_port(runner, mocker):
 
 def test__cli__custom_port(runner, mocker):
     assert_runs_with_port(runner, mocker, port=1234, args=["--port=1234"])
+
+
+def test__cli__default_run_mode(runner, mocker, monkeypatch):
+    monkeypatch.setenv("RUN_MODE", "")
+    (stub, result) = run_patched_app(runner, mocker, args=[])
+
+    assert_result_sucess(result)
+    assert_runs_with_reload(stub, True)
+
+
+def test__cli__production_run_mode(runner, mocker, monkeypatch):
+    monkeypatch.setenv("RUN_MODE", "production")
+    (stub, result) = run_patched_app(runner, mocker, args=[])
+
+    assert_result_sucess(result)
+    assert_runs_with_reload(stub, False)
