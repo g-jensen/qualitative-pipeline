51ffaf9e9b050a497e513292acd93fd188237f82
greg <greg.micah.jensen@gmail.com>
2026-09-12 20:38:30 -0400

    improves + refactors cli tests

--- quote_extraction/cli_test.py
+++ quote_extraction/cli_test.py
@@ -2,6 +2,7 @@ import cli as sut
 import pytest
 from pytest_mock import MockerFixture
 from typer.testing import CliRunner
+from typer.testing import Result as TyperResult
 from typing import Sequence
 
 @pytest.fixture
@@ -16,18 +17,36 @@ def mocker(pytestconfig: pytest.Config):
     mocker.stopall()
 
 
-def mock_unvicorn(mocker: MockerFixture, stub=None):
+def patch_unvicorn(mocker: MockerFixture, stub=None):
     mocker.patch('uvicorn.run', new=stub)
 
 
-def assert_runs_with_port(runner: CliRunner, mocker: MockerFixture, port: int | None, args: Sequence[str]):
+def run_patched_app(runner: CliRunner, mocker: MockerFixture, args: Sequence[str]):
     stub = mocker.stub()
-    mock_unvicorn(mocker,stub=stub)
-    
+    patch_unvicorn(mocker,stub=stub)
     result = runner.invoke(sut.app, args)
+    return (stub, result)
 
+
+def assert_result_sucess(result: TyperResult):
     assert result.exit_code == 0
-    stub.assert_called_once_with("api:app", host="127.0.0.1", port=port)
+
+
+def assert_runs_with_port(runner: CliRunner, mocker: MockerFixture, port: int | None, args: Sequence[str]):
+    (stub, result) = run_patched_app(runner, mocker, args)
+
+    assert_result_sucess(result)
+    assert len(stub.call_args_list) == 1
+    assert stub.call_args_list[0].kwargs["port"] == port
+
+
+def test__cli__runs_app_on_localhost(runner, mocker):
+    (stub, result) = run_patched_app(runner, mocker, args=[])
+
+    assert_result_sucess(result)
+    assert len(stub.call_args_list) == 1
+    assert stub.call_args_list[0].args == ("api:app",)
+    assert stub.call_args_list[0].kwargs["host"] == "127.0.0.1"
 
 
 def test__cli__default_port(runner, mocker):
