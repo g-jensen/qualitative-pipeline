ab2e05e364839493bc351178b99972683be783d3
greg <greg.micah.jensen@gmail.com>
2026-09-12 20:09:02 -0400

    cli runs api with port

--- quote_extraction/cli.py
+++ quote_extraction/cli.py
@@ -1,8 +1,11 @@
 import typer
+import uvicorn
+import api # Technically not necessary, but I like it. See "api:app" in this file
+
 
 app = typer.Typer()
 
 
 @app.command()
 def serve(port: int = 8080):
-    print(f"Serving on port {port}")
+    uvicorn.run("api:app", host="127.0.0.1", port=port)
--- quote_extraction/cli_test.py
+++ quote_extraction/cli_test.py
@@ -1,13 +1,38 @@
 import cli as sut
 import pytest
-import pytest_mock
+from pytest_mock import MockerFixture
 from typer.testing import CliRunner
+from typing import Sequence
+
+@pytest.fixture
+def runner():
+    return CliRunner()
 
 
-runner = CliRunner()
+@pytest.fixture
+def mocker(pytestconfig: pytest.Config):
+    mocker = MockerFixture(pytestconfig)
+    yield mocker
+    mocker.stopall()
 
 
-def test_cli():
-    result = runner.invoke(sut.app, ["--port=123"])
+def mock_unvicorn(mocker: MockerFixture, stub=None):
+    mocker.patch('uvicorn.run', new=stub)
+
+
+def assert_ran_with_port(runner: CliRunner, mocker: MockerFixture, port: int | None, args: Sequence[str]):
+    stub = mocker.stub()
+    mock_unvicorn(mocker,stub=stub)
+    
+    result = runner.invoke(sut.app, args)
+
     assert result.exit_code == 0
-    assert "Serving on port 123\n" == result.output
+    stub.assert_called_once_with("api:app", host="127.0.0.1", port=port)
+
+
+def test__cli__default_port(runner, mocker):
+    assert_ran_with_port(runner, mocker, port=8080, args=[])
+
+
+def test__cli__custom_port(runner, mocker):
+    assert_ran_with_port(runner, mocker, port=1234, args=["--port=1234"])
